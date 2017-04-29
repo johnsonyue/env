9bbb051d517e56ce17b5acbc39ce42a100d33021
johnsonyue <johnsonyuehit@163.com>
2017-04-29 10:19:59 +0000

    20170429 peeringdb

--- analyze/geoip/ixp/run.sh
+++ analyze/geoip/ixp/run.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
-mkdir db
 
-echo "python bgplgdb.py >db/bgplgdb"
-python bgplgdb.py >db/bgplgdb
+[ $# -ne 1 ] && echo './run.sh $dst_dir' && exit
 
-echo "python wikiixp.py >db/wikiixp"
-python wikiixp.py >db/wikiixp
+dst_dir=$1
+path=$dst_dir"/tmp"
+mkdir -p $path
 
-echo "python pch.py db"
-python pch.py db
+echo "python bgplgdb.py >$path/bgplgdb"
+python bgplgdb.py >$path/bgplgdb
+
+echo "python wikiixp.py >$path/wikiixp"
+python wikiixp.py >$path/wikiixp
+
+echo "python pch.py $path"
+python pch.py $path
+
+echo "python peeringdb.py $path"
+datetime=$( python peeringdb.py $path )
+datetime=$( echo $datetime | awk -F'-' '{print $NF}' )
+dst_path=$dst_dir"/"$datetime
+
+echo "mv $path $dst_path"
+mv $path $dst_path
