e0f22295a01f539748f485c266c20cd8e723f0c4
johnsonyue <johnsonyuehit@163.com>
2017-06-30 12:46:46 +0000

    generate paths from edge file

---
+++ analyze/model_test.py
@@ -0,0 +1,43 @@
+class Node():
+	def __init__(self, ip):
+		self.ip = ip
+		self.is_router = True
+		self.is_blank = False
+class Edge():
+	def __init__(self, ind_in, ind_out, delay):
+		self.ind_in = ind_in
+		self.ind_out = ind_out
+		self.delay = delay
+		self.is_direct = True
+		self.num = 1
+
+#node is indexed by ip
+#edge is index by tuple: (ingress_index, outgress_index)
+class Graph():
+	def __init__(self):
+		self.nodes = [] #adjacent list
+		self.edges = [] #edge list
+		self.edge_dict = {}
+		self.node_dict = {} #dictionaries for quick look up.
+	
+	def add_node(self, node):
+		if (not self.node_dict.has_key(node.ip)):
+			index=len(self.nodes)
+			self.node_dict[node.ip]=index
+			self.nodes.append(node)
+			return index
+		else:
+			return self.node_dict[node.ip]
+	
+	def add_edge(self, edge):
+		if ( not self.edge_dict.has_key((edge.ind_in, edge.ind_out)) ):
+			index=len(self.edges)
+			self.edges.append(edge)
+			self.edge_dict[(edge.ind_in, edge.ind_out)] = index
+			ind_in = edge.ind_in
+			ind_out = edge.ind_out
+			return index
+		else:
+			index = self.edge_dict[(edge.ind_in, edge.ind_out)]
+			self.edges[index].num+=1
+			return index
